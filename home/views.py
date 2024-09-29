3b706659c381bc7f341f694e951ddbb8f7b97a27
sergiohscl <sergiohscl2@gmail.com>
2024-09-29 17:24:32 -0300

    :boom: adjustments carrousel

--- home/views.py
+++ home/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from doctors.models import PerfilDoctor
 
 
 def home(request):
-    return render(request, 'home/home_live.html')
+    medicos = PerfilDoctor.objects.all()[:6]
+    medicos_grouped = list(group_queryset(medicos, 3))
+    return render(request, 'home/home_live.html', {'medicos_grouped': medicos_grouped}) # noqa E501
+
+
+def group_queryset(queryset, n):
+    """Agrupa a queryset em blocos de tamanho n"""
+    for i in range(0, len(queryset), n):
+        yield queryset[i:i + n]
+
+
+def carousel_view(request):
+    # Busca os 6 primeiros médicos do banco de dados
+    medicos = PerfilDoctor.objects.all()[:6]
+    # Agrupa os médicos em grupos de 3
+    medicos_grouped = list(group_queryset(medicos, 3))
+
+    return render(request, 'partials/carousel.html', {'medicos_grouped': medicos_grouped}) # noqa E501
